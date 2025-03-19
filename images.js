0dad967b8974b72bd27c57b67ab505c64363f283
johnokwute <134557852+johnokwute@users.noreply.github.com>
2025-03-19 22:36:10 +0100

    Add files via upload

---
+++ images.js
@@ -0,0 +1,14 @@
+const images = [
+    {            
+        image:'abigail1.jpeg.JPG',
+        title:'Abigail picture'
+    },
+
+    {            
+        image:'IMG_20240725_140001.jpg',
+        title:'Zoe picture'
+
+    },
+
+    
+]
---
+++ message.js
@@ -0,0 +1,14 @@
+const message1 = "i am addicted to programming";
+const message2 = "life is all about commitment";
+const age = 45;
+
+export {message1,message2, age};
+
+const msg = ()  =>{
+    const sex="male";
+    const ages=45;
+
+    return "my sex and age is "+sex+"  "+ages;
+};
+
+export default msg;
